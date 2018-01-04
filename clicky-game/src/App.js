3add72f8780f53708926de300d283d2f38dfffa6
AlexWetzel <wetzela@live.com>
2018-01-04 17:10:04 -0500

    Added comments

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -6,17 +6,19 @@ import './App.css';
 import pictures from "./pictures.json";
 
 class App extends Component {
-  // Set the state of the pictures and the counter
+  // Set the state of the pictures, the counter, and the message
   state = {
     pictures,
     count: 0,
     message: "Click a character to start the game!"
   };
-
+  // Shuffle the pictures each turn
   shuffle = () => {
+    // Copy the state of the pictures to be shuffled
     let shuffledPictures = this.state.pictures;
 
     // Fisher-Yates shuffle
+    //=============================================
     let m = shuffledPictures.length, t, i;
 
     // While there remain elements to shuffle…
@@ -29,74 +31,81 @@ class App extends Component {
       t = shuffledPictures[m];
       shuffledPictures[m] = shuffledPictures[i];
       shuffledPictures[i] = t;
+    //==============================================
     }
-
+    // Set the state to the shuffled pictures
     this.setState({ pictures: shuffledPictures })
   }
 
+  // Reset the game
   reset = () => {
-    console.log("reset is called")
+    // Set the counter to 0
     this.setState({ count: 0 });
-    console.log(this.state.count);
+    // Mark the pictures as not clicked
     this.state.pictures.map(picture => picture.isClicked = false);
   }
 
+  // Increase the score and mark the clicked panel as clicked
   goodGuess = id => {
+    // Copy the count state to a variable
     let c = this.state.count
+    // Increment the count by 1
     c++
-
+    // Search the state for the clicked picture
     let pic = this.state.pictures.filter(picture => picture.id === id)
 
+    // Reassign the array as a single object
     pic = pic[0]
-
+    // Mark the picture as clicked
     pic.isClicked = true;
+    // Find the index of the picture in the state
     const index = this.state.pictures.findIndex((picture) => picture.id === pic.id);
+    // Update the picture at its index
     const updatedPictures = update(this.state.pictures, {$splice: [[index, 1, pic]]});
+    // Set the new state
     this.setState({ pictures: updatedPictures });
 
+    // If the count reaches 12, all pictures have been clicked
     if (c >= 12) {
-
       this.setState({ message: "You did it! Click again to start over." })
     } else {
       this.setState({ message: "Good guess!" })
     }
+    // Set the new count
     this.setState({ count: c })
   }
 
   // Method that determines the game logic based on if a panel has been clicked
-  guess = (id, isClicked) => {
-  
-    // If a panel has been clicked, the game is reset
-
+  guess = (id, isClicked) => {    
+    // If the panel has been clicked before
     if (isClicked === true) {
-
+      // After the score reaches 12, it is displayed as 12 instead of resetting to 0
       if (this.state.count >= 12) {
-
-
+        // The score is reset...
         let gameWin = new Promise((resolve, reject) => {
           this.reset();
           resolve();
         });
-
+        // Then is immediately incremented for the user's guess
         gameWin.then(() => {
           this.goodGuess(id);
         });
 
       }
+      // If the panel has been clicked, and is clicked again, the game is reset
       else {
       this.setState({ message: "Bad guess! You have to start over." })
       this.reset()
       }
     }
-    // If a panel has not been clicked, the counter goes up
+    // If the panel has not been clicked before, the counter goes up
     else {     
       this.goodGuess(id);
     }
-
+    // Panels are shuffled after each guess
     this.shuffle();
   }
 
-      // Counter goes up if a clicked picture has not been clicked before
   render() {
     return(
       <div>
@@ -143,8 +152,5 @@ class App extends Component {
     )
   }
 }
-
-
-
 
 export default App;
--- clicky-game/src/components/Counter.js
+++ clicky-game/src/components/Counter.js
@@ -1,13 +1,10 @@
 import React from "react";
 
-// By extending the React.Component class, Counter inherits functionality from it
 const Counter = props => (
 
-  // The render method returns the JSX that should be rendered
-
-      <div className="card text-white bg-danger col-3">
-        <div className="card-body h3">Score: {props.count}/12</div>
-      </div>
+  <div className="card text-white bg-danger col-3">
+    <div className="card-body h3">Score: {props.count}/12</div>
+  </div>
 
 );
 
