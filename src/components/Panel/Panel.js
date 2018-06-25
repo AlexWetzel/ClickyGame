24309d87172c80c60e61746062027e338b667ef8
AlexWetzel <wetzela@live.com>
2018-06-24 22:03:12 -0400

    Improved the position of some page elements

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Panel.css";
 
 const Panel = props => (
-
-	<div className="col-4 col-md-3 panel" key={props.id} onClick={() => props.guess(props.id, props.clicked)}>
+	
+	<div className="col-4 col-md-3 panel mx-auto" key={props.id} onClick={() => props.guess(props.id, props.clicked)}>
 		<img src={props.url} alt={props.name} />
 	</div>
 
